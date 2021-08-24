15929450f3712c85e1cdf4bed6cd9ff178b7f814
Rishabh-Sharma11 <rishabhsharma1130@gmail.com>
2021-08-24 14:54:15 +0530

    Add to Cart functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/cartActions'
 
-const CartScreen = () => {
-    return (
-        <div>
-            Cart
-        </div>
-    )
+const CartScreen = ({ match, location, history }) => {
+    const productId = match.params.id
+
+    //location.search gives whatever there is after that question mark, e.g., if initially quantity is 1, 
+    //then it gives ?qty=1 bcz that's how it is present there in params.
+    //Now if we split this ?qty=1 from '=', it would give us array ['?qty', '1']; ie, 1 is present at index 1.
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+    const { cartItems } = cart
+
+    console.log(cartItems)
+
+    useEffect(() => {
+        if(productId) {
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
+    
+    return <div>Cart</div>
 }
 
 export default CartScreen
