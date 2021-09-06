5b20f5b5ca7ef28f6aeb9d088ffb4dcf9d53dabb
Rishabh-Sharma11 <rishabhsharma1130@gmail.com>
2021-09-06 11:21:19 +0530

    Admin Create Product added

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,7 +4,12 @@ import { Table, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listProducts, deleteProduct } from '../actions/productActions'
+import {
+    listProducts,
+    deleteProduct,
+    createProduct,
+} from '../actions/productActions'
+import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 
 const ProductListScreen = ({ history, match }) => {
     const dispatch = useDispatch()
@@ -19,18 +24,40 @@ const ProductListScreen = ({ history, match }) => {
         success: successDelete
     } = productDelete
 
+    const productCreate = useSelector(state => state.productCreate)
+    const {
+        loading: loadingCreate,
+        error: errorCreate,
+        success: successCreate,
+        product: createdProduct
+    } = productCreate
+
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
     useEffect(() => {
-        if (userInfo && userInfo.isAdmin) {
-            dispatch(listProducts())
-        } else {
+        dispatch({ type: PRODUCT_CREATE_RESET })
+
+        if (!userInfo.isAdmin) {
             history.push('/login')
         }
+
+        if (successCreate) {
+            history.push(`/admin/product/${createdProduct._id}/edit`)
+        } else {
+            dispatch(listProducts())
+        }
+
         //passed successDelete as a dependency in useEffect, so that when that happens, this runs again and
         //it lists the products and then it'll have the deleted product gone.
-    }, [dispatch, userInfo, history, successDelete])
+    }, [
+        dispatch,
+        userInfo,
+        history,
+        successDelete,
+        successCreate,
+        createdProduct
+    ])
 
     const deleteHandler = (id) => {
         if (window.confirm('Are you sure')) {
@@ -38,8 +65,8 @@ const ProductListScreen = ({ history, match }) => {
         }
     }
 
-    const createProductHandler = (product) => {
-        //CREATE PRODUCT
+    const createProductHandler = () => {
+        dispatch(createProduct())
     }
 
     return (
@@ -56,6 +83,8 @@ const ProductListScreen = ({ history, match }) => {
             </Row>
             {loadingDelete && <Loader />}
             {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+            {loadingCreate && <Loader />}
+            {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
             {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
                 <Table striped bordered hover responsive className='table-sm'>
                     <thead>
