d13d5abfdbfd6580e61be7ca975d39b18ce8536f
Rishabh-Sharma11 <rishabhsharma1130@gmail.com>
2021-09-07 10:36:58 +0530

    Admin functionality for marking order as delivered added

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -61,12 +61,31 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
     if (order) {
         order.isPaid = true
         order.paidAt = Date.now()
-        order.paymentResult  = {    //This stuff is going to be added from PayPal
+        order.paymentResult = {    //This stuff is going to be added from PayPal
             id: req.body.id,
             status: req.body.status,
             update_time: req.body.update_time,
             email_address: req.body.payer.email_address
         }
+
+        const updatedOrder = await order.save()
+
+        res.json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+// @desc    Update order to delivered
+// @route   GET /api/orders/:id/deliver
+// @access  Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if (order) {
+        order.isDelivered = true
+        order.deliveredAt = Date.now()
 
         const updatedOrder = await order.save()
 
@@ -93,4 +112,11 @@ const getOrders = asyncHandler(async (req, res) => {
     res.json(orders)
 })
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders, getOrders }
+export {
+    addOrderItems,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToDelivered,
+    getMyOrders,
+    getOrders
+}
