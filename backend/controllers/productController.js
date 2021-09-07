42b43604004dca0620cc5d874dd07f8e22321a64
Rishabh-Sharma11 <rishabhsharma1130@gmail.com>
2021-09-07 16:56:20 +0530

    Product pagination functionality added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -9,6 +9,9 @@ import Product from '../models/productModel.js'
 // @route   GET /api/products
 // @access  Public (This is a public route, meaning anyone can hit it)
 const getProducts = asyncHandler(async (req, res) => {
+    const pageSize = 10
+    const page = Number(req.query.pageNumber) || 1
+
     const keyword = req.query.keyword ? {
         name: {
             $regex: req.query.keyword,
@@ -16,9 +19,10 @@ const getProducts = asyncHandler(async (req, res) => {
         }
     } : {}
 
-    const products = await Product.find({ ...keyword });
+    const count = await Product.countDocuments({ ...keyword })
+    const products = await Product.find({ ...keyword }).limit(pageSize).skip(pageSize * (page - 1))
 
-    res.json(products)
+    res.json({ products, page, pages: Math.ceil(count / pageSize) })
 })
 
 // @desc    Fetch single product
